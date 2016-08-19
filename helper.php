59df1bc385b3105bad19c577dcbb0c46086af8f6
Volkmar Volli Schlothauer <GHSVS-de@users.noreply.github.com>
2016-08-19 04:51:47 +0200

    Add copyright

--- helper.php
+++ helper.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * @package plugin.system hyphenateghsvs.helper for Joomla!
+ * @version See hyphenateghsvs.xml
+ * @author G@HService Berlin Neukölln, Volkmar Volli Schlothauer
+ * @copyright Copyright (C) 2016, G@HService Berlin Neukölln, Volkmar Volli Schlothauer. All rights reserved.
+ * @license For plugin: See License.txt. For Hyphenator.js: See LICENSE_Hyphenator.txt.
+ * @authorUrl http://www.ghsvs.de
+ * @authorEmail hyphenateghsvs @ ghsvs.de
+ * @link https://github.com/GHSVS-de/plg_system_hyphenateghsvs
+ * @link ghsvs.de/programmierer-schnipsel/joomla/191-joomla-silbentrennungs-plugin-hyphenateghsvs-fuer-doofe-browser
+ */
+?>
+<?php
 defined('JPATH_BASE') or die;
 
 class PlgHyphenateGhsvsHelper
