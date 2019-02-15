cda6f620a41259b6c9adced438471d20797a78be
ReLater <ReLater@users.noreply.github.com>
2019-02-15 17:49:35 +0100

    Update version.php

--- fields/version.php
+++ fields/version.php
@@ -1,12 +1,20 @@
 <?php
+/**
+ * @package plugin.system hyphenateghsvs for Joomla!
+ * @version See hyphenateghsvs.xml
+ * @author G@HService Berlin Neukölln, Volkmar Volli Schlothauer
+ * @copyright Copyright (C) 2016-2019, G@HService Berlin Neukölln, Volkmar Volli Schlothauer. All rights reserved.
+ * @license GNU General Public License version 3 or later; see LICENSE.txt; see also LICENSE_Hyphenator.txt; see also LICENSE_Hyphenopoly.txt
+ * @authorUrl https://www.ghsvs.de
+ * @link https://github.com/GHSVS-de/plg_system_hyphenateghsvs
+ */
 /*
 GHSVS 2019-02-01
 Usage:
 <field name="assetsbe" type="plgSystemHyphenateGhsvs.assetsbe" hidden="true"
 	loadjs="false" loadcss="true" />
 
-If attributs loadjs or loadcss are missing their default value is TRUE => Assets will be loaded.	
-
+If attributs loadjs or loadcss are missing their default value is TRUE => Assets will be loaded.
 */
 defined('JPATH_PLATFORM') or die;
 
