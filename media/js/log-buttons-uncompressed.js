e90cdf845ce1ef1cec0a08da53ca1c0c8715fa8b
ReLater <ReLater@users.noreply.github.com>
2019-03-10 00:57:37 +0100

    Vanilla

--- media/js/log-buttons-uncompressed.js
+++ media/js/log-buttons-uncompressed.js
@@ -2,43 +2,43 @@ plg_system_hyphenateghsvs = window.plg_system_hyphenateghsvs || {};
 
 (function (document, plg_system_hyphenateghsvs)
 {
-  'use strict';
+	'use strict';
 
-  var logButtonsEvents = function logButtonsEvents(callback)
+	var logButtonsEvents = function logButtonsEvents(callback)
 	{
-	  var logButtonsContainer = document.getElementById("logButtonsContainer");
-    var logButtonsOutput    = logButtonsContainer.querySelector(".ajaxOutput");
+		var logButtonsContainer = document.getElementById("logButtonsContainer");
+		var logButtonsOutput    = logButtonsContainer.querySelector(".ajaxOutput");
 
-    logButtonsContainer.addEventListener("click", function (event)
+		logButtonsContainer.addEventListener("click", function (event)
 		{
-      if (event.target.classList.contains("deleteFile"))
+			if (event.target.classList.contains("deleteFile"))
 			{
-        event.preventDefault();
+				event.preventDefault();
 				logButtonsOutput.innerHTML = "";
-        callback({
-          plugin: "PlgSystemHyphenateGhsvsDeleteLogFile",
+				callback({
+          			plugin: "PlgSystemHyphenateGhsvsDeleteLogFile",
 					ajaxOutput: logButtonsOutput
-        });
-      }
+        			});
+      			}
 			else if (event.target.classList.contains('showFilePath'))
 			{
-        event.preventDefault();
+				event.preventDefault();
 				logButtonsOutput.innerHTML = '';
-        callback({
-          plugin: 'PlgSystemHyphenateGhsvsShowLogFilePath',
+				callback({
+					plugin: 'PlgSystemHyphenateGhsvsShowLogFilePath',
 					ajaxOutput: logButtonsOutput
-        });
-      }
+				});
+      			}
 			else if (event.target.classList.contains('showFile'))
 			{
-        event.preventDefault();
+        			event.preventDefault();
 				logButtonsOutput.innerHTML = '';
-        callback({
-          plugin: 'PlgSystemHyphenateGhsvsShowLogFile',
+        			callback({
+          				plugin: 'PlgSystemHyphenateGhsvsShowLogFile',
 					ajaxOutput: logButtonsOutput
-        });
-      }
-    });
-  };
+        			});
+      			}
+    		});
+  	};
 	logButtonsEvents(plg_system_hyphenateghsvs.getJson);
 })(document, plg_system_hyphenateghsvs);
