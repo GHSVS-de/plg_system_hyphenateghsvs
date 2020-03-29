653cc94c71f343533cba19a57fd669cce94a9278
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2020-03-29 17:15:52 +0200

    Make uninstallation possible if custom requirements don't match.
    Uninstallation was not possible after update 3.9.10 to 4.0

--- installerScript.php
+++ installerScript.php
@@ -37,42 +37,40 @@ class plgSystemHyphenateGhsvsInstallerScript extends InstallerScript
 
 	public function preflight($type, $parent)
 	{
-		if ($type === 'update')
-		{
-			$this->removeOldUpdateservers();
-		}
-
 		$manifest = @$parent->getManifest();
 
 		if ($manifest instanceof SimpleXMLElement)
 		{
-			$minimumPhp = trim((string) $manifest->minimumPhp);
-			$minimumJoomla = trim((string) $manifest->minimumJoomla);
-
-			// Custom
-			$maximumPhp = trim((string) $manifest->maximumPhp);
-			$maximumJoomla = trim((string) $manifest->maximumJoomla);
-
-			$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
-			$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
-
-			if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+			if ($type === 'update' || $type === 'install' || $type === 'discover_install')
 			{
-				$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
-				Log::add($msg, Log::WARNING, 'jerror');
-			}
+				$minimumPhp = trim((string) $manifest->minimumPhp);
+				$minimumJoomla = trim((string) $manifest->minimumJoomla);
 
-			// Check for the maximum PHP version before continuing
-			if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
-			{
-				$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
+				// Custom
+				$maximumPhp = trim((string) $manifest->maximumPhp);
+				$maximumJoomla = trim((string) $manifest->maximumJoomla);
 
-				Log::add($msg, Log::WARNING, 'jerror');
-			}
+				$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
+				$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
 
-			if (isset($msg))
-			{
-				return false;
+				if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+				{
+					$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
+					Log::add($msg, Log::WARNING, 'jerror');
+				}
+
+				// Check for the maximum PHP version before continuing
+				if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
+				{
+					$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
+	
+					Log::add($msg, Log::WARNING, 'jerror');
+				}
+
+				if (isset($msg))
+				{
+					return false;
+				}
 			}
 
 			if (trim((string) $manifest->allowDowngrades))
@@ -85,7 +83,12 @@ class plgSystemHyphenateGhsvsInstallerScript extends InstallerScript
 		{
 			return false;
 		}
-		
+
+		if ($type === 'update')
+		{
+			$this->removeOldUpdateservers();
+		}
+
 		return true;
 	}
 
